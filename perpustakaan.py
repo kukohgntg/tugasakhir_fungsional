66ccf9d981e08b5b6655adc5ad496f0b88f15008
kukohgntg <kukohfr@gmail.com>
2023-12-19 01:01:32 +0700

    fix daftar buku

--- perpustakaan.py
+++ perpustakaan.py
@@ -1,27 +1,34 @@
 import os
 
 # Pure Functions
+
+
 def clear_screen():
     os.system("CLS")
+
 
 def read_books_data():
     with open("daftarbuku.txt", "r") as bukadata:
         return bukadata.readlines()
 
+
 def write_books_data(data):
     with open("daftarbuku.txt", "w") as bukadata:
         bukadata.writelines(data)
+
 
 def read_peminjam_data():
     with open("daftarpeminjam.txt", "r") as bukadata:
         return bukadata.readlines()
 
+
 def write_peminjam_data(data):
     with open("daftarpeminjam.txt", "w") as bukadata:
         bukadata.writelines(data)
 
+
 # Lambda Expressions
-print_menu = lambda: (
+def print_menu(): return (
     print("     SELAMAT DATANG DI PROGRAM PERDIG"),
     print("\nPilih daftar menu untuk mengakses program :\n"),
     print("[1] Lihat Daftar Buku"),
@@ -36,22 +43,29 @@ print_menu = lambda: (
 )
 
 # List Comprehension
+
+
 def display_books():
     books_data = read_books_data()
     sorted_books = sorted(books_data)
     return [f"\n{i}. {book}" for i, book in enumerate(sorted_books, start=1)] if sorted_books else ["\n[Data tidak tersedia]"]
+
 
 def search_book(title):
     books_data = read_books_data()
     return [book for book in books_data if title in book]
 
 # Iterator dan Generator
+
+
 def menu():
     clear_screen()
     print_menu()
     return int(input("\nMasukkan kode menu yang ingin diakses : "))
 
 # Higher Order Function
+
+
 def select_menu_action(choice):
     menu_actions = {
         1: daftarbuku,
@@ -67,16 +81,25 @@ def select_menu_action(choice):
     return menu_actions.get(choice, lambda: print("\n[Kode yang anda masukkan tidak valid!]"))
 
 # Built-in Higher Order Functions
+
+
 def process_menu_choice(choice):
     action = select_menu_action(choice)
     action()
 
 # Currying
+
+
 def input_with_message(message):
     return input(message)
 
-input_title = lambda: input_with_message("Masukkan judul buku yang ingin dicari : ")
-input_data = lambda message: input_with_message(message)
+
+def input_title(): return input_with_message(
+    "Masukkan judul buku yang ingin dicari : ")
+
+
+def input_data(message): return input_with_message(message)
+
 
 def tambah_data():
     judul = input_data("Judul Buku	: ")
@@ -87,10 +110,13 @@ def tambah_data():
     write_books_data(bukadata)
     print("\n[Data Buku Berhasil Ditambahkan]")
 
+
 def input_baru():
     return input_title(), input_data("Masukkan data baru\nJudul Buku	: "), input_data("Penulis Buku	: "), input_data("Tahun Terbit	: ")
 
 # Inner Function
+
+
 def ubah_data():
     def update_book_data(title, judulbr, penulisbr, tahunbr):
         return ",".join([judulbr, penulisbr, tahunbr + "\n"]) if title in data_buku else data_buku
@@ -99,9 +125,11 @@ def ubah_data():
     title, judulbr, penulisbr, tahunbr = input_baru()
     bukadata = read_books_data()
     data_buku = [book for book in bukadata if title in book][0].split(",")
-    bukadata = [update_book_data(title, judulbr, penulisbr, tahunbr) for data_buku in bukadata]
+    bukadata = [update_book_data(title, judulbr, penulisbr, tahunbr)
+                for data_buku in bukadata]
     write_books_data(bukadata)
     print("\n[Data Buku Berhasil Diubah]")
+
 
 def hapus_data():
     str = input_title()
@@ -111,10 +139,13 @@ def hapus_data():
     print("\n[Data Buku Telah Terhapus]")
 
 # Closure
+
+
 def display_peminjam():
     peminjam_data = read_peminjam_data()
     sorted_peminjam = sorted(peminjam_data)
     return [f"\n{i}. {peminjam}" for i, peminjam in enumerate(sorted_peminjam, start=1)] if sorted_peminjam else ["\n[Data tidak tersedia]"]
+
 
 def tambah_peminjam():
     nama = input_data("Nama		   : ")
@@ -125,6 +156,7 @@ def tambah_peminjam():
     write_peminjam_data(peminjam_data)
     print("\n[Data Peminjam Berhasil Ditambahkan]")
 
+
 def hapus_peminjam():
     str = input_data("Masukkan Nama Peminjam yang Ingin Dihapus : ")
     peminjam_data = read_peminjam_data()
@@ -133,6 +165,8 @@ def hapus_peminjam():
     print("\n[Data Peminjam Telah Terhapus]")
 
 # Decorators
+
+
 def display_and_return(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
@@ -143,19 +177,26 @@ def display_and_return(func):
     return wrapper
 
 # Applying Decorators
+
+
 @display_and_return
 def daftarbuku():
-    display_books()
+    books_list = display_books()
+    for book in books_list:
+        print(book)
+
 
 @display_and_return
 def caridata():
-    result = search_book(input_title())
+    title = input_title()
+    result = search_book(title)
     if result:
         print(f"\nJudul Buku	: {result[0]}")
         print(f"Penulis		: {result[1]}")
         print(f"Tahun Terbit	: {result[2]}")
     else:
         print("\n[Data tidak ditemukan]")
+
 
 @display_and_return
 def tambahdata():
@@ -164,12 +205,14 @@ def tambahdata():
     if tmbhdata.lower() == "ya":
         tambahdata()
 
+
 @display_and_return
 def ubahdata():
     ubah_data()
     ubhdata = input_data("\nIngin mengubah data buku lagi? (Ya/Tidak) : ")
     if ubhdata.lower() == "ya":
         ubahdata()
+
 
 @display_and_return
 def hapusdata():
@@ -178,23 +221,31 @@ def hapusdata():
     if hpsdata.lower() == "ya":
         hapusdata()
 
+
 @display_and_return
 def daftarpeminjam():
-    display_peminjam()
+    peminjam_list = display_peminjam()
+    for peminjam in peminjam_list:
+        print(peminjam)
+
 
 @display_and_return
 def tambahpeminjam():
     tambah_peminjam()
-    tmbhpeminjam = input_data("\nIngin menambahkan data peminjam lagi? (Ya/Tidak) : ")
+    tmbhpeminjam = input_data(
+        "\nIngin menambahkan data peminjam lagi? (Ya/Tidak) : ")
     if tmbhpeminjam.lower() == "ya":
         tambahpeminjam()
+
 
 @display_and_return
 def hapuspeminjam():
     hapus_peminjam()
-    hpspeminjam = input_data("\nIngin menghapus data peminjam lagi? (Ya/Tidak) : ")
+    hpspeminjam = input_data(
+        "\nIngin menghapus data peminjam lagi? (Ya/Tidak) : ")
     if hpspeminjam.lower() == "ya":
         hapuspeminjam()
+
 
 # Main Program
 if __name__ == "__main__":
